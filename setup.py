88f9db6697bc0a51803e901bf8707fe8cef831e2
Neeraj Kumar <suneerajkumar@gmail.com>
2025-01-08 18:15:50 +0530

    setup commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+
+setup(
+    name="shipment",
+    version="0.0.1",
+    author="Neeraj",
+    author_email="neerajkumar1092005@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
